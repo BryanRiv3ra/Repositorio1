9711c2c1cc2054dd66cd4a74861e9eb54ad8b199
Bryan <bryanjosueriverahernandez@gmail.com>
2024-02-13 22:17:31 -0600

    Primer commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ConsoleApp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿int numeroEntero = 65;
+char caracter = Convert.ToChar(numeroEntero);
+Console.WriteLine("El numero entero " + numeroEntero + " es equivalente al caracter: " + caracter);
+
+int entero = 5;
+decimal valordecimal = entero;
+Console.WriteLine("El numero entero " + entero + " es equivalente al decimal: " + valordecimal);
+
+float numeroFloat = 3.14f;
+int valorentero = (int)numeroFloat;
+Console.WriteLine("El dato float " + numeroFloat + " es equivalente al entero: " + valorentero);
+
