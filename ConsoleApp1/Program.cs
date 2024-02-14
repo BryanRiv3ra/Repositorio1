f72509baaecf378d14cc8129268b8e96dcf20cc6
Bryan <bryanjosueriverahernandez@gmail.com>
2024-02-13 22:28:14 -0600

    Segundo commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,3 +10,10 @@ float numeroFloat = 3.14f;
 int valorentero = (int)numeroFloat;
 Console.WriteLine("El dato float " + numeroFloat + " es equivalente al entero: " + valorentero);
 
+char letra = 'A';
+int valorNumerico = (int)letra; 
+Console.WriteLine("El valor eqivalente de " + letra + " a valor numerico es " + valorNumerico);
+
+decimal numeroDecimal = 3.1416m;
+float numeroFlotante = (float)numeroDecimal;
+Console.WriteLine("El valor " + numeroDecimal + " su equivalente en tipo float es " + numeroFlotante);
